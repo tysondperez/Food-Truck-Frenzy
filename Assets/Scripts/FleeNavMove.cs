69d93e04c7f8c8f09af1b96075e4360000ebb94c
alaharp <harper4@wwu.edu>
2024-12-09 12:14:42 -0800

    flee ai nav update

--- Assets/Scripts/FleeNavMove.cs
+++ Assets/Scripts/FleeNavMove.cs
@@ -9,16 +9,49 @@ public class FleeNavMove : MonoBehaviour
     NavMeshAgent agent;
     Animator animator;
 
+    [SerializeField] Transform[] navPoints;
+    float fleeRadius = 200f;
+    float maxDistance;
+    Transform bestPoint;
+    float playerDistance;
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
 
+        GetPoint();
     }
 
     void Update()
     {
         animator.SetFloat("Speed", agent.speed);
-        agent.SetDestination(transform.position - (player.position - transform.position));
+        //agent.SetDestination(transform.position - (player.position - transform.position));
+
+        if (bestPoint != null)
+        {
+            if (Vector3.Distance(transform.position, bestPoint.position) < 2f)
+            {
+                GetPoint();
+            }
+            agent.SetDestination(bestPoint.position);
+        }
+    }
+
+    void GetPoint()
+    {
+        foreach (Transform t in navPoints)
+        {
+            playerDistance = Vector3.Distance(t.position, player.position);
+
+            if (playerDistance > fleeRadius)
+            {
+                if(playerDistance > maxDistance)
+                {
+                    maxDistance = playerDistance;
+                    bestPoint = t;
+                }
+            }
+        }
     }
 }
